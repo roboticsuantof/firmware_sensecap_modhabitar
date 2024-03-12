020e41de7b0a9dee3f3118e5740eb7fc18386649
Giovanni Troncoso Bordoni <giovanni.troncosob@gmail.com>
2024-03-12 15:24:52 -0300

    Adds SEN54 sensor, Adds comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
 #include <Arduino.h>
 #include "sensorBuilder.hpp"
 
+#define FW_VERSION "1.0.0"
+
 SensorBuilderClass SensorBuilder;
 
 void setup()
 {
   Serial.begin(115200);
   SensorBuilder.check_grove();
-
+  // SensorBulder class modbus address starts at 4 (DEC), the other address are for device data.
+  // Temperature using MCP9808 uses address 4
   sensorTemperature *pTempSensor = new sensorTemperature();
   SensorBuilder.addSensor(pTempSensor);
   
+  // Mutigas uses 4 modbus address: 6(No2) 8(C2H5OH) 10(VOC) 12(CO)
   sensorMultiGas* pMultiGasSensor = new sensorMultiGas();
   SensorBuilder.addSensor(pMultiGasSensor);
+
+  // SEN54 sensor uses 4 modbus address: 14(Temp) 16(RH) 18(VOC) 20(PM1.0) 22(PM2.5) 24(PM10.0)
+  sensorEnvironmental* pEnviromentalS54Sensor = new sensorEnvironmental();
+  SensorBuilder.addSensor(pEnviromentalS54Sensor);
 
   SensorBuilder.begin();
 }
@@ -20,5 +28,4 @@ void setup()
 void loop()
 {
   SensorBuilder.poll();
-  delay(100);
 }
